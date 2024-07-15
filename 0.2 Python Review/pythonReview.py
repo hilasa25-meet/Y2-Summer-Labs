863464735f3331befbe6bbc2a7d90afb14da9f69
drorsh23-meet <109904739+drorsh23-meet@users.noreply.github.com>
2024-07-15 14:19:15 +0300

    a youtube video code

---
+++ 0.2 Python Review/pythonReview.py
@@ -0,0 +1,23 @@
+def create_youtube_video(title, description , comment_text, username)
+
+likes>=0
+dislikes>=0
+comments = {"username":username, "comment": comment_text}
+ytvid = {"title": title , "description": description}
+emotions = {"likes": likes, "dislikes": dislikes, "comments": comments}
+
+	for i in range(10)
+	print(i)
+
+
+def like(ytvid)
+likes.append(1)
+
+def dislikes(ytvid)
+dislikes.append(1)
+
+def add_comment(youtubevideo, username, comment_text)
+	for i in range(10)
+	print(i)
+new_youtube_video = {"title": my lovely dog, "description": my dog doing back roll, "username": hila, "comment_text": lol, "likes": 4, 
+"dislikes": 0}	
---
+++ 2.1 python review #1/python_review.py
@@ -0,0 +1,20 @@
+import randomn
+number = random.randint(26,40)
+temperatures = [30, 32, 27, 38, 33, 29,31]
+days_of_the_week = ["sunday", "monday", "tuesday", "wendsday", "thersday", "friday", "saterday"]
+print("sunday," "monday," "wendsday")
+good_days_count=3 
+highest_temp=38
+highest_temp_day="wendsday"
+lowest_temp=27
+lowest_temp_day="tuesday"
+above.avg=[32, 33, 38]
+
+print("temperaturesfor the week: sunday-30, monday- 32, tuesday- 27, wendsday- 38, thersday-33, friday- 29, saterday- 31")
+print("good days count-3")
+print("lowest temperature of the week- 27, highest temperature of the week- 38")
+print("average temperature- 31.248")
+print("days with temperaturesabove the average- monday, thersday, wendsday-")
+
+sorted_temp=[27, 29, 30, 31, 32, 33, 38]
+print("sorted_temp")
